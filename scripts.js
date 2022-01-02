ac2d7be06f2f08708ce4fdfc65682a95953fa14b
DJ de Vlamingh <embryonic.dev@gmail.com>
2022-01-02 14:53:23 +0100

    create f game()

--- scripts.js
+++ scripts.js
@@ -3,3 +3,37 @@ const gameOptions = ["Rock", "Paper", "Scissors"]
 function computerPlay() {
   return(gameOptions[Math.floor(Math.random() * gameOptions.length)]);
 }
+
+function userPlay() {
+  return(prompt("Choose: Rock, Paper, or Scissors to Play,"));
+}
+
+function playRound(playerSelection, computerSelection) {
+  if(((playerSelection == "Paper" && computerSelection == "Rock")) || (playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Rock" && computerSelection == "Scissors")) {
+    userScore += 1;
+    console.log(`You Win this Round! ${playerSelection} Beats ${computerSelection}...`);
+    console.log(`Your Score = ${userScore}, Computer Score = ${computerScore}`);
+    console.log("");
+  } else if(((playerSelection == "Rock" && computerSelection == "Paper")) || (playerSelection == "Paper" && computerSelection == "Scissors") || (playerSelection == "Scissors" && computerSelection == "Rock")) {
+    computerScore += 1;
+    console.log(`You Lose this Round! ${computerSelection} Beats ${playerSelection}...`);
+    console.log(`Your Score = ${userScore}, Computer Score = ${computerScore}`);
+    console.log("");
+  } else if(computerSelection == playerSelection) {
+    console.log("It's a Tie! Let's Try Again...");
+    console.log("");
+  } else {
+      console.log("You did not Choose a Valid Option! Concentrate, & Try Again!");
+      console.log("");
+      game();
+  }
+}
+
+function game() {
+  let playerSelection = userPlay();
+  let computerSelection = computerPlay();
+  console.log("Player Selection: " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1,).toLocaleLowerCase());
+  console.log("Computer Selection: " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1,).toLocaleLowerCase());
+  // console.log(playRound(playerSelection, computerSelection));
+  playRound(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1,).toLocaleLowerCase(), computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1,).toLocaleLowerCase());
+}
